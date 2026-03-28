d2a8dfceafe0ba9d1aba3351bef25ce2fb710969
IliasAlhandiui <IliasAlhandiui@gmail.com>
2026-03-28 01:15:42 +0100

    Initialize RustyCache project with basic cache functionality and file handling

---
+++ src/main.rs
@@ -0,0 +1,69 @@
+mod rusty_cache;
+use rusty_cache::{Cache, CacheValues};
+use std::io::{self, Write};
+
+fn main() {
+    let output_file  = String::from("data/SAVE.txt");
+    let mut cache = Cache::new(output_file);
+    println!("RustyCache v0.1 (Startup Mode)");
+    
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let parts: Vec<&str> = input.trim().split_whitespace().collect();
+        if parts.is_empty() {
+            continue;
+        }
+
+        match parts[0].to_uppercase().as_str() {
+            "INSERT" => {
+                if parts.len() < 3 {
+                    continue;
+                }
+
+                let key = parts[1].to_string();
+                let value_raw = parts[2..].join(" ");
+
+                let value = match value_raw.parse::<i32>() {
+                    Ok(number) => CacheValues::Number(number),
+                    Err(_) => CacheValues::Text(value_raw.to_string()),
+                };
+
+                cache.insert(key, value);
+                println!("OK");
+            }
+            "GET" => {
+                if parts.len() < 2 {
+                    continue;
+                }
+                match cache.get(parts[1]) {
+                    Some(val) => println!("{:?}", val),
+                    None => println!("(nil)"),
+                }
+            }
+            "DELETE" => {
+                if parts.len() < 2 {
+                    continue;
+                }
+                match cache.remove(parts[1]) {
+                    Some(val) => println!("REMOVED: {:?}", val),
+                    None => println!("NOTHING REMOVED"),
+                }
+            }
+            "SAVE" => {
+                match cache.write_to_file() {
+                    Ok(_) => println!("Cache saved to file."),
+                    Err(e) => println!("Error saving cache: {}", e),
+                }
+            }
+            "EXIT" => break,
+            _ => println!("Unknown command. Try SET or GET."),
+        }
+    }
+}
---
+++ src/rusty_cache.rs
@@ -0,0 +1,48 @@
+use std::{collections::HashMap, fs, io};
+
+#[derive(Debug)]
+pub enum CacheValues {
+    Text(String),
+    Number(i32),
+}
+
+pub struct Cache {
+    pub storage: HashMap<String, CacheValues>,
+    output_file_path: String
+}
+
+impl Cache {
+    pub fn new(file: String) -> Self {
+        Cache {
+            storage: HashMap::new(),
+            output_file_path: file
+        }
+    }
+
+    pub fn insert(&mut self, key: String, value: CacheValues) {
+        self.storage.insert(key, value);
+    }
+
+    pub fn get(&self, key: &str) -> Option<&CacheValues> {
+        self.storage.get(key)
+    }
+
+    pub fn remove(&mut self, key:&str) -> Option<CacheValues> {
+        self.storage.remove(key)
+
+    }
+
+    pub fn write_to_file(&self) -> io::Result<()> {
+        let mut contents = String::new();
+        for (key, value) in &self.storage {
+            let value_str = match value {
+                CacheValues::Text(s) => s.clone(),
+                CacheValues::Number(n) => n.to_string(),
+            };
+            contents.push_str(&format!("{}: {}\n", key, value_str));
+        }
+        fs::write(&self.output_file_path, contents)?;
+        Ok(())
+    }
+
+}
